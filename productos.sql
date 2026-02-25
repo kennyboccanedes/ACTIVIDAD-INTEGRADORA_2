fcb3af3941430f51f1690f9585727b013cb05d2e
kennyboccanedes <bocanedeske@gmail.com>
2026-02-24 21:43:36 -0500

    Delete productos.sql

--- productos.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE inventario;
-USE inventario;
-CREATE TABLE productos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    stock INT NOT NULL,
-    precio DECIMAL(10,2) NOT NULL
-);
-INSERT INTO productos (nombre, stock, precio) VALUES ('Producto ejemplo', 5, 10.00);
